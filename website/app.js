fef663dc2425e637712748af06c88f6adfd9eb12
malakmaurice <43122053+malakmaurice@users.noreply.github.com>
2021-07-14 11:52:30 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,5 +1,80 @@
 /* Global Variables */
 
+const { copyFileSync } = require("fs");
+
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+// api url and key from openweathermap
+const APIKEY = "e970177e9f1c23ca62f214ddb1efba59";
+const url = "https://api.openweathermap.org/data/2.5/weather?zip=";
+
+// sendDate function to localServer 
+const sendData = async (path = "", data = {}) => {
+  const res = await fetch(path, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const responseData = await res.json();
+    return responseData;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+// addEventListener with click event 
+document.getElementById("generate").addEventListener("click", generatefun);
+
+function generatefun(e) {
+  // get elements by id 
+  cityZip = document.getElementById("zip").value;
+  feelings = document.getElementById("feelings").value;
+// first get data from API then store it in server then display it 
+  getData(url, cityZip, APIKEY)
+    .then(function (data) {
+      sendData("/sendData", {
+        tempreture: data.main.temp,
+        feelings: feelings,
+        newDate: newDate
+      });
+      displayUI()
+
+    })
+
+
+}
+
+
+
+
+const getData = async (APIurl, cityZip, APIkey) => {
+
+
+  const response = await fetch(APIurl + cityZip +"&appid="+ APIkey);
+
+  try {
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+const displayUI = async () => {
+  const request = await fetch("/");
+  try {
+    const data = await request.json();
+    // getElements by id and add the content in innerhtml 
+    document.getElementById('content').innerHTML = "you feel like:" + data[data.length - 1].feelings;
+    document.getElementById("temp").innerHTML = " the temp is: " + data[data.length - 1].tempreture;
+    document.getElementById("date").innerHTML = "the date is: " + data[data.length - 1].newDate;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
