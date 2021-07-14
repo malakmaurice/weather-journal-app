b4021907a9e7133608b55d01a47a102035e4202e
malakmaurice <43122053+malakmaurice@users.noreply.github.com>
2021-07-14 11:09:01 +0200

    setup server

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// Require Express to run server and routes
-
+// setup express to use 
+const express =require('express');
+// instance of app from express
+const app = express();
+const cors = require('cors');
+app.use(cors());
 // Start up an instance of app
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require("body-parser");
+const { log } = require('console');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -14,6 +20,6 @@ app.use(bodyParser.json());
 
 // Initialize the main project folder
 app.use(express.static('website'));
-
-
+app.get('/',function(request,response){response.sendFile(__dirname+"website/index.html")});
+app.listen(8000,function(){console.log("Hallloooooo");});
 // Setup Server
